5ea0e3fd4d898fc21a08f9ed5a1cfcec09c5faf8
David Op de Beeck <davidopdebeeck@hotmail.com>
2018-04-22 13:50:11 +0200

    Initial commit

---
+++ src/test/java/be/davidopdebeeck/simpleci/example/project/ExampleTest.java
@@ -0,0 +1,14 @@
+package be.davidopdebeeck.simpleci.example.project;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class ExampleTest {
+
+    @Test
+    public void exampleTest() {
+        assertThat(2 + 2, equalTo(4));
+    }
+}
