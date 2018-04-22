20d48923ae8ee228d44f9b92cff59db253aeb8ff
David Op de Beeck <davidopdebeeck@hotmail.com>
2018-04-22 19:32:52 +0200

    removed failing test

--- src/test/java/be/davidopdebeeck/simpleci/example/project/ExampleTest.java
+++ src/test/java/be/davidopdebeeck/simpleci/example/project/ExampleTest.java
@@ -11,9 +11,4 @@ public class ExampleTest {
     public void exampleTest() {
         assertThat(2 + 2, equalTo(4));
     }
-
-    @Test
-    public void exampleFailingTest() {
-        assertThat(2 + 3, equalTo(4));
-    }
 }
