83a9e20f093286bfdaafdcdf33a620de0ed47b77
David Op de Beeck <davidopdebeeck@hotmail.com>
2018-04-25 23:21:13 +0200

    added sleep in test

--- src/test/java/be/davidopdebeeck/simpleci/example/project/ExampleTest.java
+++ src/test/java/be/davidopdebeeck/simpleci/example/project/ExampleTest.java
@@ -2,13 +2,15 @@ package be.davidopdebeeck.simpleci.example.project;
 
 import org.junit.Test;
 
+import static java.lang.Thread.sleep;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class ExampleTest {
 
     @Test
-    public void exampleTest() {
+    public void exampleTest() throws InterruptedException {
         assertThat(2 + 2, equalTo(4));
+        sleep(10000);
     }
 }
