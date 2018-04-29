7a5061a6aba6f352771ebe66ae688149412c4505
David Op de Beeck <davidopdebeeck@hotmail.com>
2018-04-29 20:28:18 +0200

    fixed main class

--- src/main/java/be/davidopdebeeck/simpleci/example/project/Application.java
+++ src/main/java/be/davidopdebeeck/simpleci/example/project/Application.java
@@ -1,7 +1,12 @@
 package be.davidopdebeeck.simpleci.example.project;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplication(Application.class).run(args);
+    }
 }
